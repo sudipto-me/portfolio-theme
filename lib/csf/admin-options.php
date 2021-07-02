aef3302741dbc9f3d28448540ada675051262dd5
sudipto <sudipto004.ruet@gmail.com>
2021-07-02 19:57:38 +0600

    theme options updated

--- lib/csf/admin-options.php
+++ lib/csf/admin-options.php
@@ -1,111 +1,137 @@
 <?php
 // Control core classes for avoid errors
 if ( class_exists( 'CSF' ) ) {
-    // Set a unique slug-like ID
-    $prefix = 'portfolio';
-    // Create options
-    CSF::createOptions( $prefix, array(
-        'framework_title' => esc_html__( 'Theme Options', 'portfolio' ),
-        'menu_title'      => esc_html__( 'Theme Options', 'portfolio' ),
-        'menu_slug'       => 'portfolio-options',
-        'theme'           => 'light'
-    ) );
-    
-    // Create a section
-    CSF::createSection( $prefix, array(
-        'title'  => 'Header',
-        'fields' => array(
-            // favicon
-            array(
-                'type'    => 'content',
-                'content' => 'To upload favicon go to <strong>Appearance->Customize->Site Identity->Site Icon</strong>',
-                'title'   => 'Favicon',
-            ),
-            // header logo
-            array(
-                'id'    => 'theme-header-logo',
-                'type'  => 'media',
-                'title' => 'Site Logo',
-            ),
-            array(
-                'id' => 'theme-cta',
-                'type' => 'text',
-                'title' => __('My Account Page URL','portfolio')
-            ),
-            array(
-                'id'    => 'theme-header-script',
-                'type'  => 'code_editor',
-                'title' => esc_html__( 'Header Script', 'biermann' ),
-            )
-        )
-    ) );
-    // footer
-    CSF::createSection( $prefix, array(
-        'title'  => 'Footer',
-        'fields' => array(
-            // footer bg image
-            array(
-                'id'    => 'theme-footer-bg',
-                'type'  => 'media',
-                'title' => 'Footer Background Image',
-            ),
-            // copyright
-            array(
-                'id'    => 'theme-copyright-content',
-                'type'  => 'wp_editor',
-                'title' => esc_html__( 'Copyright content', 'portfolio' ),
-            ),
-            //footer javascript
-            array(
-                'id'    => 'theme-footer-script',
-                'type'  => 'code_editor',
-                'title' => esc_html__( 'Footer Script', 'portfolio' ),
-            )
-        )
-    ) );
-    
-    // Create social section
-    CSF::createSection( $prefix, array(
-        'title'  => 'Social',
-        'fields' => array(
-            array(
-                'id'     => 'theme-social-repeater',
-                'type'   => 'repeater',
-                'title'  => 'Social Links',
-                'fields' => array(
-                    array(
-                        'id'    => 'social-icon',
-                        'type'  => 'text',
-                        'title' => 'Icon'
-                    ),
-                    array(
-                        'id'    => 'social-link',
-                        'type'  => 'text',
-                        'title' => 'Link'
-                    ),
-                ),
-            ),
-        )
-    ) );
-    // Create Color section
-    CSF::createSection( $prefix, array(
-        'title'  => 'Color',
-        'fields' => array(
-            array(
-                'id'    => 'theme-primary-color',
-                'type'  => 'color',
-                'title' => 'Theme Primary Color',
-            ),
-            array(
-                'id'    => 'theme-secondary-color',
-                'type'  => 'color',
-                'title' => 'Theme Secondary Color',
-            ),
-            array(
-                'id'    => 'banner-overlay-color',
-                'type'  => 'color',
-                'title' => 'Hero Section Overlay Color',
-            )
-        )
-    ) );
+	// Set a unique slug-like ID
+	$prefix = 'portfolio';
+	// Create options
+	CSF::createOptions( $prefix, array(
+		'framework_title' => esc_html__( 'Theme Options', 'portfolio' ),
+		'menu_title'      => esc_html__( 'Theme Options', 'portfolio' ),
+		'menu_slug'       => 'theme-options',
+		'theme'           => 'light'
+	) );
+
+	// Create home section
+	CSF::createSection( $prefix, array(
+		'title'  => esc_attr__( 'Header', 'portfolio' ),
+		'fields' => array(
+			// favicon
+			array(
+				'type'    => 'content',
+				/* translators: Appearance->Customize->Site Identity->Site Icon */
+				'content' => sprintf( __( 'To upload favicon go to <strong>%s</strong>', 'portfolio' ), 'Appearance->Customize->Site Identity->Site Icon' ),
+				'title'   => 'Favicon',
+			),
+			// header logo
+			array(
+				'id'    => 'theme-header-logo',
+				'type'  => 'media',
+				'title' => esc_html__( 'Site Logo', 'portfolio' ),
+			),
+			array(
+				'id'    => 'theme-header-script',
+				'type'  => 'code_editor',
+				'title' => esc_html__( 'Header Script', 'portfolio' ),
+			)
+		)
+	) );
+	// contact and address section
+	CSF::createSection( $prefix, array(
+		'title'  => esc_attr__( 'Contact', 'portfolio' ),
+		'fields' => array(
+			// phone
+			array(
+				'id'    => 'contact-phone',
+				'type'  => 'text',
+				'title' => esc_html__( 'Contact Phone Number', 'portfolio' )
+			),
+			// email
+			array(
+				'id'    => 'contact-email',
+				'type'  => 'text',
+				'title' => esc_html__( 'Contact Email Address', 'portfolio' )
+			),
+			// skype
+			array(
+				'id'    => 'contact-skype',
+				'type'  => 'text',
+				'title' => esc_html__( 'Contact Skype', 'portfolio' )
+			),
+			// address
+			array(
+				'id'    => 'contact-address',
+				'type'  => 'wp_editor',
+				'title' => esc_html__( 'Contact Address', 'portfolio' )
+			)
+		)
+	) );
+	// footer
+	CSF::createSection( $prefix, array(
+		'title'  => esc_attr__( 'Footer', 'portfolio' ),
+		'fields' => array(
+			// footer bg image
+			array(
+				'id'    => 'theme-footer-bg',
+				'type'  => 'media',
+				'title' => esc_html__( 'Footer Background Image', 'portfolio' ),
+			),
+			// copyright
+			array(
+				'id'    => 'theme-copyright-content',
+				'type'  => 'wp_editor',
+				'title' => esc_html__( 'Copyright content', 'portfolio' ),
+			),
+			//footer javascript
+			array(
+				'id'    => 'theme-footer-script',
+				'type'  => 'code_editor',
+				'title' => esc_html__( 'Footer Script', 'portfolio' ),
+			)
+		)
+	) );
+
+	// Create social section
+	CSF::createSection( $prefix, array(
+		'title'  => esc_attr__( 'Social', 'portfolio' ),
+		'fields' => array(
+			array(
+				'id'     => 'theme-social-repeater',
+				'type'   => 'repeater',
+				'title'  => esc_html__( 'Social Links', 'portfolio' ),
+				'fields' => array(
+					array(
+						'id'    => 'social-icon',
+						'type'  => 'text',
+						'title' => esc_html__( 'Icon', 'portfolio' ),
+					),
+					array(
+						'id'    => 'social-link',
+						'type'  => 'text',
+						'title' => esc_html__( 'Link', 'portfolio' ),
+					),
+				),
+			),
+		)
+	) );
+	// Create Color section
+	CSF::createSection( $prefix, array(
+		'title'  => esc_attr__( 'Color', 'portfolio' ),
+		'fields' => array(
+			array(
+				'id'    => 'theme-primary-color',
+				'type'  => 'color',
+				'title' => esc_html__( 'Theme Primary Color', 'portfolio' ),
+			),
+			array(
+				'id'    => 'theme-secondary-color',
+				'type'  => 'color',
+				'title' => esc_html__( 'Theme Secondary Color', 'portfolio' ),
+			),
+			array(
+				'id'    => 'banner-overlay-color',
+				'type'  => 'color',
+				'title' => esc_html__( 'Hero Section Overlay Color', 'portfolio' ),
+			)
+		)
+	) );
 }
