76dc174f1df3e9a72322e0f1b44f5ba0587d3b1f
Sudipto Shakhari <Sudipto004.ruet@gmail.com>
2022-02-23 17:47:00 +0600

    update

---
+++ deploy-scripts/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Print commands to the screen
+set -x
+
+set -eu
+
+if [[ -z "$PRIVATE_KEY" ]]; then
+    echo "PRIVATE_KEY is not defined" 1>&2
+    exit 1
+fi
+
+if [[ -z "$USERNAME" ]]; then
+    echo "USERNAME is not defined" 1>&2
+    exit 1
+fi
+
+if [[ -z "$SERVER_IP" ]]; then
+    echo "SERVER_IP is not defined" 1>&2
+    exit 1
+fi
+
+if [[ -z "$SERVER_DESTINATION" ]]; then
+    echo "SERVER_DESTINATION is not defined" 1>&2
+    exit 1
+fi
+
+SSHPATH="$HOME/.ssh"
+mkdir -p "$SSHPATH"
+echo "$PRIVATE_KEY" > "$SSHPATH/key"
+chmod 600 "$SSHPATH/key"
+SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
+
+# Copy files.
+sh -c "rsync -vrxc --no-group --no-owner --delete -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no' $FOLDER $SERVER_DEPLOY_STRING --exclude-from=./deploy-scripts/distignore.txt"
+
+# Stop printing commands to the screen.
+set +x
